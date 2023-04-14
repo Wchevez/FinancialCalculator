127e7ef29f91b0e4a941bb383737e9ed0ea6621b
William <will2144@icloud.com>
2023-04-14 14:54:53 -0400

    Added a new file

---
+++ src/main/java/com/learntocode/FinancialCalculator.java
@@ -0,0 +1,11 @@
+package com.learntocode;
+
+import java.util.Scanner;
+
+public class FinancialCalculator {
+    public static void main(String[] args) {
+    }
+    Scanner scanner = new Scanner(System.in);
+    System.out.print
+
+}
